8099f8d0fe5338656801c04efbcd18fa4880333a
Anastasiia-Niii <a.nistratova14@gmail.com>
2024-02-26 10:02:10 +0300

    fix: delete sass, add fonts

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Promocodes from "../Tables/Promocodes";
 import "./Tabs.css";
 import Ambassadors from "../Tables/Ambassadors";
@@ -11,17 +12,29 @@ import BudgetPrice from "../Tables/BudgetPrice";
 
 function Tabs() {
   const { pathname } = useLocation();
+  const theme = createTheme({
+    typography: {
+      fontFamily: [
+        'YS Text',
+        'Arial',
+        'sans-serif',
+      ].join(','),
+    },
+  });
 
   return (
     <div className="tabs">
-      {pathname.includes("/promocodes") && <Promocodes></Promocodes>}
-      {pathname.includes("/ambassadors") && <Ambassadors></Ambassadors>}
-      {pathname.includes("/content") && <Content></Content>}
-      {pathname.includes("/send-merch") && <SendMerch></SendMerch>}
-      {pathname.includes("/budget/info") && <Budget></Budget>}
-      {pathname.includes("/budget/price") && <BudgetPrice></BudgetPrice>}
-      {pathname.includes("/loyalty-programm") && <Loyalti></Loyalti>}
-      {pathname.includes("/faq") && <FAQ></FAQ>}
+      <ThemeProvider theme={theme}>
+        {pathname.includes("/promocodes") && <Promocodes></Promocodes>}
+        {pathname.includes("/ambassadors") && <Ambassadors></Ambassadors>}
+        {pathname.includes("/content") && <Content></Content>}
+        {pathname.includes("/send-merch") && <SendMerch></SendMerch>}
+        {pathname.includes("/budget/info") && <Budget></Budget>}
+        {pathname.includes("/budget/price") && <BudgetPrice></BudgetPrice>}
+        {pathname.includes("/loyalty-programm") && <Loyalti></Loyalti>}
+        {pathname.includes("/faq") && <FAQ></FAQ>}
+      </ThemeProvider>
+
     </div>
   );
 }
