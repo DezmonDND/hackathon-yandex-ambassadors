411ce9f05eb2fb06c3354143f683669e04f03e63
Olesia Erastova <olesia1205@yandex.ru>
2024-03-07 14:07:57 +0300

    feat(components): add ContentContent for Popup

--- src/components/Popup/PopupContent/ContentContent/ContentContent.js
+++ src/components/Popup/PopupContent/ContentContent/ContentContent.js
@@ -1,9 +1,48 @@
+import { useState } from 'react';
 import "./ContentContent.css";
+import CardContent from '../../../CardContent/CardContent';
+import { cardGaidData, cardContentData} from "../../../../mocks/card-content-data";
 
 function ContentContent() {
+  const [cardsContent, setCardsContent] = useState(cardContentData);
+  const [cardsGaid, setCardsGaid] = useState(cardGaidData);
+
+  const handleDeleteCard = (index, type) => {
+    if (type === 'content') {
+      const updatedCardsContent = cardsContent.filter((_, i) => i !== index);
+      setCardsContent(updatedCardsContent);
+    } else if (type === 'gaid') {
+      const updatedCardsGaid = cardsGaid.filter((_, i) => i !== index);
+      setCardsGaid(updatedCardsGaid);
+    }
+  };
+
   return (
-    <div className="content__container">
-      <h1>Content</h1>
+    <div className="contentContent__container">
+      <div className="contentContent__info-block">
+        <h2 className="contentContent__subtitle">Гайд</h2>
+        {cardsGaid.map((card, i) => (
+          <CardContent
+            key={i}
+            card={card}
+            index={i}
+            type= "gaid"
+            handleDeleteCard={handleDeleteCard}
+          />
+        ))}
+      </div>
+      <div className="contentContent__info-block">
+        <h2 className="contentContent__subtitle">Контент</h2>
+        {cardsContent.map((card, i) => (
+          <CardContent
+            key={i}
+            card={card}
+            index={i}
+            type="content"
+            handleDeleteCard={handleDeleteCard}
+          />
+        ))}
+      </div>
     </div>
   );
 }
