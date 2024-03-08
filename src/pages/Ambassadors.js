62b76912754c3194f9ec75e80c73c542c40bb31a
unknown <denedoseikin@mail.ru>
2024-03-09 00:55:11 +0700

    fix: добавить зависимости

--- src/pages/Ambassadors.js
+++ src/pages/Ambassadors.js
@@ -333,7 +333,7 @@ export default function Ambassadors({
     apiTables.getAmbassadors().then((ambassadors) => {
       setRows(ambassadors);
     });
-  }, []);
+  }, [setRows]);
 
   return (
     <Layout>
