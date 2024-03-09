fdacd59cd73fb0d2724aabc2112eb0cdcedd4501
Olesia Erastova <olesia1205@yandex.ru>
2024-03-09 14:48:47 +0300

    fix(styles): fix class vs className

--- src/components/Popup/PopupContent/FormContent/FormContent.js
+++ src/components/Popup/PopupContent/FormContent/FormContent.js
@@ -7,22 +7,22 @@ function FormContent() {
         <h2 className="formContent__subtitle">
           Контакты
         </h2>
-        <ul class="formContent__table-list">
-          <li class="formContent__table-item">
-            <span class="formContent__text">ФИО</span>
-            <span class="formContent__text formContent__data">Иванов Иван Иванович</span>
+        <ul className="formContent__table-list">
+          <li className="formContent__table-item">
+            <span className="formContent__text">ФИО</span>
+            <span className="formContent__text formContent__data">Иванов Иван Иванович</span>
           </li>
-          <li class="formContent__table-item">
-            <span class="formContent__text">Email</span>
-            <span class="formContent__text formContent__data">email@example.com</span>
+          <li className="formContent__table-item">
+            <span className="formContent__text">Email</span>
+            <span className="formContent__text formContent__data">email@example.com</span>
           </li>
-          <li class="formContent__table-item">
-            <span class="formContent__text">Телефон</span>
-            <span class="formContent__text formContent__data">+7 (999) 999 09 09</span>
+          <li className="formContent__table-item">
+            <span className="formContent__text">Телефон</span>
+            <span className="formContent__text formContent__data">+7 (999) 999 09 09</span>
           </li>
-          <li class="formContent__table-item">
-            <span class="formContent__text">Телеграм</span>
-            <span class="formContent__text formContent__data">@nickname</span>
+          <li className="formContent__table-item">
+            <span className="formContent__text">Телеграм</span>
+            <span className="formContent__text formContent__data">@nickname</span>
           </li>
         </ul>
       </div>
@@ -30,28 +30,28 @@ function FormContent() {
         <h2 className="formContent__subtitle">
           Об участнике
         </h2>
-        <ul class="formContent__table-list">
-          <li class="formContent__table-item">
-            <span class="formContent__text">Программа</span>
-            <span class="formContent__text formContent__data formContent__data-about">Продакт-менеджер для специалистов с опытом</span>
+        <ul className="formContent__table-list">
+          <li className="formContent__table-item">
+            <span className="formContent__text">Программа</span>
+            <span className="formContent__text formContent__data formContent__data-about">Продакт-менеджер для специалистов с опытом</span>
           </li>
-          <li class="formContent__table-item">
-            <span class="formContent__text">Образование</span>
-            <span class="formContent__text formContent__data formContent__data-about">РГГУ, филология</span>
+          <li className="formContent__table-item">
+            <span className="formContent__text">Образование</span>
+            <span className="formContent__text formContent__data formContent__data-about">РГГУ, филология</span>
           </li>
-          <li class="formContent__table-item">
-            <span class="formContent__text">Работа</span>
-            <span class="formContent__text formContent__data formContent__data-about">проджект-менеджер в ООО Айтипроекты</span>
+          <li className="formContent__table-item">
+            <span className="formContent__text">Работа</span>
+            <span className="formContent__text formContent__data formContent__data-about">проджект-менеджер в ООО Айтипроекты</span>
           </li>
-          <li class="formContent__table-item">
-            <span class="formContent__text">Цель в Практикуме</span>
-            <span class="formContent__text formContent__data formContent__data-about">
+          <li className="formContent__table-item">
+            <span className="formContent__text">Цель в Практикуме</span>
+            <span className="formContent__text formContent__data formContent__data-about">
               Углубление имеющихся знаний, чтобы использовать их в текущей работе
             </span>
           </li>
-          <li class="formContent__table-item">
-            <span class="formContent__text">Цель амбассадора</span>
-            <span class="formContent__text formContent__data formContent__data-about">Вести блог, Снимать видео</span>
+          <li className="formContent__table-item">
+            <span className="formContent__text">Цель амбассадора</span>
+            <span className="formContent__text formContent__data formContent__data-about">Вести блог, Снимать видео</span>
           </li>
         </ul>
       </div>
--- src/components/Popup/PopupContent/MerchContent/MerchContent.js
+++ src/components/Popup/PopupContent/MerchContent/MerchContent.js
@@ -6,14 +6,14 @@ function MerchContent() {
   return (
     <div className="merchContent__container">
       <div className="merchContent__info-block">
-        <ul class="merchContent__table-list">
-          <li class="merchContent__table-item">
+        <ul className="merchContent__table-list">
+          <li className="merchContent__table-item">
             <h2 className="merchContent__subtitle">Промокод</h2>
-            <span class="merchContent__text merchContent__data">samplepromocode13</span>
+            <span className="merchContent__text merchContent__data">samplepromocode13</span>
           </li>
           {Object.entries(promoData).map(([key, value]) => (
             <li className="merchContent__table-item" key={key}>
-              <span class="merchContent__text">{key}</span>
+              <span className="merchContent__text">{key}</span>
               <span className="merchContent__text merchContent__data">{value}</span>
             </li>
           ))}
@@ -21,32 +21,32 @@ function MerchContent() {
       </div>
       <AdressForm />
       <div className="merchContent__info-block">
-        <ul class="merchContent__table-list">
-          <li class="merchContent__table-item">
+        <ul className="merchContent__table-list">
+          <li className="merchContent__table-item">
             <h2 className="merchContent__subtitle">Мерч</h2>
           </li>
           {Object.entries(merchData).map(([key, value]) => (
             <li className="merchContent__table-item" key={key}>
-              <span class="merchContent__text">{key}</span>
+              <span className="merchContent__text">{key}</span>
               <span className="merchContent__text merchContent__data_size">{value}</span>
             </li>
           ))}
         </ul>
       </div>
-      <ul class="merchContent__table-list">
-        <li class="merchContent__table-item">
+      <ul className="merchContent__table-list">
+        <li className="merchContent__table-item">
           <h2 className="merchContent__subtitle">Программа лояльности</h2>
         </li>
-        <ul class="merchContent__table-list_merch">
+        <ul className="merchContent__table-list_merch">
           {Object.entries(programmData).map(([key, value]) => (
             <li className="merchContent__table-item" key={key}>
-              <span class="merchContent__text_merch">{key}</span>
+              <span className="merchContent__text_merch">{key}</span>
               <span className="merchContent__text_merch merchContent__data_merch">{value}</span>
             </li>
           ))}
         </ul>
       </ul>
-      <div class="merchContent__table-list">
+      <div className="merchContent__table-list">
         <h2 className="merchContent__subtitle">Комментарий</h2>
         <form>
           <textarea
